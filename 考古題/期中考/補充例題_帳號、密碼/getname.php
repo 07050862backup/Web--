9ba971a9ad55cf7fb47abd409cc4c97c845ba77e
07050862backup <110028879+07050862backup@users.noreply.github.com>
2022-07-26 18:45:06 +0800

    Add files via upload

---
+++ 考古題/期中考/補充例題_帳號、密碼/getname.php
@@ -0,0 +1,11 @@
+
+<?php
+
+$name = $_GET['username'];
+$pass = $_GET['passwd'];
+
+echo $name;
+echo "的密碼是";
+echo $pass;
+ 
+?>
---
+++ 考古題/期中考/補充例題_帳號、密碼/postname.php
@@ -0,0 +1,11 @@
+
+<?php
+
+$name = $_POST['username'];
+$pass = $_POST['passwd'];
+
+echo $name;
+echo "的密碼是";
+echo $pass;
+ 
+?>
